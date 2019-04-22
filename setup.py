f7c4c7483927963da9348b30aeafc4088707c995
smr1020@gmail.com <smr1020@gmail.com>
2019-04-22 11:43:05 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='biogeo',
+      version='0.1',
+      description='Calculate anthropogenic carbon and other common conservative and non conservative tracers',
+      author='Sean Ridge',
+      author_email='sridge@ldeo.columbia.edu',
+      license='MIT',
+      packages=['biogeo'],
+      zip_safe=False)
